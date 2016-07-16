52c594a2d5759430bf49288f5b65b0a4a0327836
Curtis Mitchell <curtis.l.mitchell@gmail.com>
2016-07-16 14:33:36 -0700

    First React component; npm start script

--- app/index.js
+++ app/index.js
@@ -1,2 +1,15 @@
-const app = document.getElementById('app');
-app.innerHTML = 'Hello!';
+const React = require('react');
+const ReactDom = require('react-dom');
+
+const HelloWorld = React.createClass({
+  render: function () {
+    return (
+      <div> Hello React Program </div>
+    )
+  }
+});
+
+ReactDom.render(
+  <HelloWorld />,
+  document.getElementById('app')
+);
