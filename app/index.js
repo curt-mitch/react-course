1aa92947fe31eae03343515c625775348c4154cb
Curtis Mitchell <curtis.l.mitchell@gmail.com>
2016-07-16 15:06:26 -0700

    Created nested components in avatar component

--- app/index.js
+++ app/index.js
@@ -1,15 +1,51 @@
 const React = require('react');
 const ReactDom = require('react-dom');
 
-const HelloWorld = React.createClass({
+const USER_DATA = {
+  name: 'Curtis Mitchell',
+  username: 'curt-mitch',
+  image: 'https://avatars3.githubusercontent.com/u/4998605?v=3&s=140'
+};
+
+const ProfilePic = React.createClass({
+  render: function () {
+    return <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
+  }
+});
+
+const ProfileLink = React.createClass({
   render: function () {
     return (
-      <div> Hello React Program </div>
+      <div>
+        <a href={'https://www.github.com/' + this.props.username}>
+          {this.props.username}
+        </a>
+      </div>
+    )
+  }
+});
+
+const ProfileName = React.createClass({
+  render: function () {
+    return (
+      <div>{this.props.name}</div>
+    )
+  }
+});
+
+const Avatar = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <ProfilePic imageUrl={this.props.user.image} />
+        <ProfileName name={this.props.user.name} />
+        <ProfileLink username={this.props.user.username} />
+      </div>
     )
   }
 });
 
 ReactDom.render(
-  <HelloWorld />,
+  <Avatar user={USER_DATA} />,
   document.getElementById('app')
 );
